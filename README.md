11e6ae443f12a43cfe3910c13c2a6afeff4ddb12
Polina Prokopenko <polina@Mac.station>
2025-10-17 23:50:34 +0200

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# How to use (later)
+# How to use
+
+## How to run
+
+Install dependencies and run:  
+
+``` bash
+npm i && npm run start
+```
+
+## Default size
+
+Widget is for 1920x1080. Width: 1850; height: 160.
+
+## How to change values
+
+Open *index.js* and change values in first part of the file. There are comments. On each change save file to update the widget in OBS.  
--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+// Current progress values
 let value = 4184; 
-let goal = 8000; 
+
+// Goal value
+let goal = 8000;
+
+// Label for the progress bar
 let label = "На работу «Искры»";
 
-
+// Calculate percentage (don't touch this part)
 let percentage = value / goal * 100;
-
 if (percentage > 100) {
   percentage = 100;
 }
 
-
+// Change widget elements after page load
 document.addEventListener("DOMContentLoaded", () => {
   let inner = document.getElementById("progress-bar_inner");
   let labelElem = document.getElementById("progress-bar_label");
