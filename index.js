7521d73d6b3abdf9e557f273dcc43492d50b1d4e
Polina Prokopenko <polina@Mac.station>
2025-10-25 22:56:31 +0200

    added a few labels

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // Current progress values
-let value = 4184; 
+let value = 2537 + 12 + 5; 
 
 // Goal value
 let goal = 8000;
 
 // Label for the progress bar
-let label = "На работу «Искры»";
+// let label = "На работу «Искры»";
+let label = "Гере на восстановление режима сна";
+// let label = "На освобождение Полины из подвала";
+// let label = "";
 
 // Calculate percentage (don't touch this part)
 let percentage = value / goal * 100;
